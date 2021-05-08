1cabbc90d14be184ca6716ae3f936c65a21adfd5
Sowmya-Korada <66238781+Sowmya-Korada@users.noreply.github.com>
2021-05-08 15:58:57 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,14 +1,19 @@
-const startingMinutes = 10;
+const startingMinutes = prompt("How many minutes timer do you want:");
 let time = startingMinutes*60;
 const count = document.getElementById('countdown');
 
-setInterval(updateCountdown,1000);
+var x=setInterval(updateCountdown,1000);
+
 
 function updateCountdown(){
+    document.getElementById("min").innerHTML=`${startingMinutes} Minutes Timer`;
     const minutes=Math.floor(time/60);
     let seconds=time % 60;
     seconds= seconds < 10 ? '0' + seconds : seconds;
     count.innerHTML=`${minutes}:${seconds}`;
     time--;
+    if(minutes==0 && seconds==0){
+       clearInterval(x);
+    }
 }
 
